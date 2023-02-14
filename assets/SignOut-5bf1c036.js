33b62de513ce65a71678d7f6320be528e92cddfc
Doan_na <doanna@mpos.vn>
2023-02-14 10:23:30 +0700

    deploy

---
+++ assets/SignOut-5bf1c036.js
@@ -0,0 +1,1 @@
+import{n as e,u as o,y as a,o as c,a as n,b as u}from"./index-9bb6db20.js";const r={class:"w-50 m-auto p-5"},_=u("h4",{class:"text-center mb-3"},"Sign Out Page",-1),i=[_],d={__name:"SignOut",setup(h){const t=e(),{signOutAuth:s}=o();return a(async()=>{console.log("sign out"),s(),t.push("/")}),(l,p)=>(c(),n("div",r,i))}};export{d as default};
